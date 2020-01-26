c2beb238dff67d00ebf1e959c6ae7694d9f940c1
Petr Kuznetsov <vold-world@yandex.ru>
2020-01-26 13:22:09 +0300

    *Added simple reactive number

---
+++ Core/Reactive/ReactiveNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RanterTools.Base
+{
+
+    public class ReactiveNumber<T>
+    {
+        public delegate void ValueChangeDelegate(T value);
+        public event ValueChangeDelegate OnValueChanged;
+        T value;
+        public T Value
+        {
+            get { return value; }
+            set
+            {
+                this.value = value;
+                if (OnValueChanged != null) OnValueChanged(value);
+            }
+        }
+    }
+
+}
