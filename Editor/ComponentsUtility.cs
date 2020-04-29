b659be57eeedae5e468cadd4073e0991aed64861
Petr Kuznetsov <vold-world@yandex.ru>
2020-04-29 19:02:29 +0300

    *Added select all missing script object menu item.

--- Editor/ComponentsUtility.cs
+++ Editor/ComponentsUtility.cs
@@ -29,12 +29,12 @@ namespace RanterTools.Editor.Base
                 {
                     RemoveMissingSciptsRecursive(gameObject);
                 }
+                UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(SceneManager.GetSceneAt(s));
             }
         }
         static void RemoveMissingSciptsRecursive(GameObject gameObject)
         {
             GameObjectUtility.RemoveMonoBehavioursWithMissingScript(gameObject);
-            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(gameObject.scene);
             for (int t = 0; t < gameObject.transform.childCount; t++)
             {
                 RemoveMissingSciptsRecursive(gameObject.transform.GetChild(t).gameObject);
@@ -52,6 +52,39 @@ namespace RanterTools.Editor.Base
                 var gameObject = Selection.gameObjects[i];
                 GameObjectUtility.RemoveMonoBehavioursWithMissingScript(gameObject);
                 UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(gameObject.scene);
+            }
+        }
+
+
+        /// <summary>
+        /// Select all missing scripts
+        /// </summary>
+        [MenuItem(GlobalNames.MenuRanterTools + "/" + GlobalNames.Components + "/" + GlobalNames.ComponentsSelectAllGameobjectsWithMissingScripts)]
+        static void SelectAllMissingScripts()
+        {
+            for (int s = 0; s < SceneManager.sceneCount; s++)
+            {
+                foreach (var gameObject in SceneManager.GetSceneAt(s).GetRootGameObjects())
+                {
+                    SelectMissingSciptsRecursive(gameObject);
+                }
+                UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(SceneManager.GetSceneAt(s));
+            }
+
+        }
+
+        static void SelectMissingSciptsRecursive(GameObject gameObject)
+        {
+            List<GameObject> selection = new List<GameObject>(Selection.gameObjects);
+            if (GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(gameObject) != 0)
+            {
+                selection.Add(gameObject);
+                Selection.objects = selection.ToArray();
+            }
+
+            for (int t = 0; t < gameObject.transform.childCount; t++)
+            {
+                SelectMissingSciptsRecursive(gameObject.transform.GetChild(t).gameObject);
             }
         }
         #endregion Global Methods
